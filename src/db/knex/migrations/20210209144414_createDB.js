d97abf11157280bdb2c8e3e2414e9587af371cdd
Dmytro Tyshchenko <kwazar2008@ukr.net>
2021-02-20 16:34:39 +0200

    createDB

--- src/db/knex/migrations/20210209144414_createDB.js
+++ src/db/knex/migrations/20210209144414_createDB.js
@@ -3,7 +3,7 @@ exports.up = async (knex) => {
     table.increments('id');
     table.string('name').notNullable();
     table.string('avatar').notNullable();
-    table.string('password_hash').notNullable();
+    table.string('password_hash').nullable();
     table.string('email').notNullable();
     table
       .enu('role', ['user', 'organizer', 'moderator'], {
@@ -13,116 +13,122 @@ exports.up = async (knex) => {
       .notNullable()
       .defaultTo('user');
     table.uuid('refresh_token').nullable();
+    // table.boolean('banned').notNullable().defaultTo(false);
     table.timestamp('deleted_at').nullable();
     table.timestamps();
     table.unique('email', 'users_email_unk');
+  });
+
+  await knex.schema.createTable('organizations', (table) => {
+    table.increments('id');
+    table.string('name').notNullable();
+    table.string('phones').notNullable();
+    table.string('email').notNullable();
+    // table.string('address').notNullable();
+    // table.text('description').notNullable();
+    table.integer('user_id').notNullable();
+    table.foreign('user_id', 'organizations_fk0').references('users.id');
+  });
+
+  await knex.schema.createTable('general_info', (table) => {
+    table.increments('id');
+    table.string('name').notNullable();
+    table.string('address').notNullable();
+    // table.specificType('coordinates', 'POINT').notNullable();
+    table.string('phones').nullable();
+    table.string('website').nullable();
+    table.string('main_photo').notNullable();
+    table.text('description').notNullable();
+    table.boolean('accessibility').notNullable().defaultTo(false);
+    table.boolean('dog_friendly').notNullable().defaultTo(false);
+    table.boolean('child_friendly').notNullable().defaultTo(false);
+    table.integer('user_id').notNullable();
+    table.boolean('moderated').notNullable().defaultTo(false);
+    table.timestamp('deleted_at').nullable();
+    table.timestamps();
+    table.foreign('user_id', 'general_info_fk0').references('users.id');
   });
 
   await knex.schema.createTable('places', (table) => {
     table.increments('id');
     table.string('category_id').notNullable();
     table.string('type').nullable();
-    table.string('name').notNullable();
-    table.string('phone').nullable();
-    table.string('address').notNullable();
-    table.string('website').nullable();
     table.json('work_time').notNullable();
-    table.boolean('accessibility').notNullable().defaultTo(false);
-    table.boolean('dog_friendly').notNullable().defaultTo(false);
-    table.boolean('child_friendly').notNullable().defaultTo(false);
-    table.text('about_info').notNullable();
     table.decimal('rating').notNullable().defaultTo(1.0);
-    table.timestamp('deleted_at').nullable();
-    table.timestamps();
-    table.integer('user_id').notNullable();
-    table.integer('moderated').nullable();
-    table.foreign('user_id', 'places_fk0').references('users.id');
-    table.foreign('moderated', 'places_fk1').references('users.id');
-  });
-
-  await knex.schema.createTable('places_photos', (table) => {
-    table.increments('id');
-    table.integer('place_id').notNullable();
-    table.string('url').notNullable();
-    table.foreign('place_id', 'places_photos_fk0').references('places.id');
+    table.integer('organization_id').notNullable();
+    table.foreign('organization_id', 'places_fk0').references('organizations.id');
+    table.inherits('general_info');
   });
 
   await knex.schema.createTable('reviews', (table) => {
-    table.increments('id');
+    // table.increments('id');
     table.integer('place_id').notNullable();
     table.integer('user_id').notNullable();
     table.integer('rating').notNullable();
     table.text('review_text').notNullable();
     table.foreign('place_id', 'reviews_fk0').references('places.id');
     table.foreign('user_id', 'reviews_fk1').references('users.id');
+    table.primary(['place_id', 'user_id'], 'reviews_pkey');
   });
 
-  await knex.schema.createTable('users_activity_places', (table) => {
-    table.increments('id');
+  await knex.schema.createTable('visited_places', (table) => {
     table.integer('place_id').notNullable();
     table.integer('user_id').notNullable();
-    table.boolean('visited').notNullable().defaultTo(false);
-    table.boolean('chosen').notNullable().defaultTo(false); // selected
-    table.foreign('place_id', 'users_activity_places_fk0').references('places.id');
-    table.foreign('user_id', 'users_activity_places_fk1').references('users.id');
+    table.foreign('place_id', 'visited_places_fk0').references('places.id');
+    table.foreign('user_id', 'visited_places_fk1').references('users.id');
+    table.primary(['place_id', 'user_id'], 'visited_places_pkey');
+  });
+
+  await knex.schema.createTable('favorites_places', (table) => {
+    table.integer('place_id').notNullable();
+    table.integer('user_id').notNullable();
+    table.foreign('place_id', 'favorites_places_fk0').references('places.id');
+    table.foreign('user_id', 'favorites_places_fk1').references('users.id');
+    table.primary(['place_id', 'user_id'], 'favorites_places_pkey');
   });
 
   await knex.schema.createTable('events', (table) => {
     table.increments('id');
-    table.string('name').notNullable();
-    table.string('address').notNullable();
-    table.date('date').notNullable();
-    table.time('time').notNullable();
+    table.specificType('passing_time', 'INTERVAL').notNullable();
     table.string('organizer').notNullable();
     table.integer('place_id').nullable();
-    table.string('phone').notNullable();
-    table.string('website').nullable();
-    table.boolean('accessibility').notNullable().defaultTo(false);
-    table.boolean('dog_friendly').notNullable().defaultTo(false);
     table.decimal('price').notNullable().defaultTo(0.0);
-    table.text('about_info').notNullable();
     table.text('program').notNullable();
-    table.integer('likes').notNullable().defaultTo(0);
-    table.integer('dislikes').notNullable().defaultTo(0);
-    table.timestamp('deleted_at').nullable();
-    table.timestamps();
-    table.integer('user_id').notNullable();
-    table.integer('moderated').nullable();
     table.foreign('place_id', 'events_fk0').references('places.id');
     table.foreign('user_id', 'events_fk1').references('users.id');
-    table.foreign('moderated', 'events_fk2').references('users.id');
+    table.inherits('general_info');
   });
 
-  await knex.schema.createTable('events_photos', (table) => {
-    table.increments('id');
-    table.integer('event_id').notNullable();
-    table.string('url').notNullable();
-    table.foreign('event_id', 'events_photos_fk0').references('events.id');
-  });
-
-  await knex.schema.createTable('users_activity_events', (table) => {
-    table.increments('id');
+  await knex.schema.createTable('chosen_events', (table) => {
     table.integer('event_id').notNullable();
     table.integer('user_id').notNullable();
-    table.boolean('visited').notNullable().defaultTo(false);
-    table.boolean('like').notNullable().defaultTo(false);
-    table.boolean('dislike').notNullable().defaultTo(false);
-    table.foreign('event_id', 'users_activity_events_fk0').references('events.id');
-    table.foreign('user_id', 'users_activity_events_fk1').references('users.id');
+    table.foreign('event_id', 'chosen_events_fk0').references('events.id');
+    table.foreign('user_id', 'chosen_events_fk1').references('users.id');
+    table.primary(['event_id', 'user_id'], 'chosen_events_pkey');
   });
 
-  await knex.raw(`ALTER TABLE "users_activity_events"
-    ADD CONSTRAINT "users_activity_events_like_check" CHECK(NOT ("like" AND "dislike"))`);
+  await knex.schema.createTable('photos', (table) => {
+    table.increments('id');
+    table.string('photo_url').notNullable();
+    table.string('author_name').notNullable();
+    table.string('author_link').nullable();
+    table.integer('place_id').nullable();
+    table.integer('event_id').nullable();
+    table.foreign('place_id', 'photos_fk0').references('places.id');
+    table.foreign('event_id', 'photos_fk1').references('events.id');
+  });
 };
 
 exports.down = async (knex) => {
-  await knex.schema.dropTable('users_activity_events');
-  await knex.schema.dropTable('events_photos');
+  await knex.schema.dropTable('photos');
+  await knex.schema.dropTable('chosen_events');
   await knex.schema.dropTable('events');
-  await knex.schema.dropTable('users_activity_places');
+  await knex.schema.dropTable('favorites_places');
+  await knex.schema.dropTable('visited_places');
   await knex.schema.dropTable('reviews');
-  await knex.schema.dropTable('places_photos');
   await knex.schema.dropTable('places');
+  await knex.schema.dropTable('general_info');
+  await knex.schema.dropTable('organizations');
   await knex.schema.dropTable('users');
   await knex.raw('DROP TYPE "role_type"');
 };
