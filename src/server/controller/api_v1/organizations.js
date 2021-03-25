41dad5a69673d52805b7119182fc843b72d16365
Roman-Kirnos <kirnos007@gmail.com>
2021-03-25 18:00:50 +0200

    002-s: change getAll organizations, eslint rule

--- src/server/controller/api_v1/organizations.js
+++ src/server/controller/api_v1/organizations.js
@@ -3,7 +3,17 @@ const { getOrganizations } = require('../../../db');
 async function getAll(ctx) {
   const organizations = await getOrganizations();
 
-  ctx.body = { organizations };
+  const response = {
+    approvedOrganizations: [],
+    proposedOrganizations: [],
+  };
+
+  for (const { id, name, moderated } of organizations) {
+    if (moderated) response.approvedOrganizations.push({ id, name });
+    else response.proposedOrganizations.push({ id, name });
+  }
+
+  ctx.body = response;
 }
 
 module.exports = { getAll };
