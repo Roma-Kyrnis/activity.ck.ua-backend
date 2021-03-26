dc8e0fd28c08f0dcd556c1a3ffe8dfafdaf558c9
Roman-Kirnos <kirnos007@gmail.com>
2021-03-26 10:51:03 +0200

    002: fix getAll organizations schema

--- src/server/schema/api_v1/organizations.js
+++ src/server/schema/api_v1/organizations.js
@@ -4,19 +4,16 @@ const getAll = {
   output: {
     200: {
       body: {
-        organizations: Joi.array().items(
+        approvedOrganizations: Joi.array().items(
           Joi.object({
             id: Joi.number().required(),
             name: Joi.string().min(3).max(255).required(),
-            phones: Joi.array()
-              .items(
-                Joi.string()
-                  .pattern(/^\+380\d{9}$/)
-                  .required(),
-              )
-              .required(),
-            email: Joi.string().email().required(),
-            moderated: Joi.boolean().required(),
+          }),
+        ),
+        proposedOrganizations: Joi.array().items(
+          Joi.object({
+            id: Joi.number().required(),
+            name: Joi.string().min(3).max(255).required(),
           }),
         ),
       },
