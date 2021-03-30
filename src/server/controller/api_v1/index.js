2edfa1d7b6b37bbfc527b3725f7ff0bae5f3bc86
Roman-Kirnos <kirnos007@gmail.com>
2021-03-30 09:32:55 +0300

    002.2: require storage

--- src/server/controller/api_v1/index.js
+++ src/server/controller/api_v1/index.js
@@ -1,5 +1,6 @@
 const auth = require('./auth');
 const places = require('./places');
 const organizations = require('./organizations');
+const storage = require('./storage');
 
-module.exports = { auth, places, organizations };
+module.exports = { auth, places, organizations, storage };
--- src/server/schema/api_v1/index.js
+++ src/server/schema/api_v1/index.js
@@ -1,9 +1,11 @@
 const auth = require('./auth');
 const places = require('./places');
 const organizations = require('./organizations');
+const storage = require('./storage');
 
 module.exports = {
   auth,
   places,
   organizations,
+  storage,
 };
