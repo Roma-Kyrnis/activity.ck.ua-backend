386267b99dd1ea4b57fe2dbd18a0d97a82583370
Roman-Kirnos <kirnos007@gmail.com>
2021-03-30 19:31:46 +0300

    002: delete extra function

--- src/server/middleware/errorHandler.js
+++ src/server/middleware/errorHandler.js
@@ -19,20 +19,18 @@ function setErrorResponse(incomingError, ctx) {
   }
 }
 
-async function handler(ctx, next) {
-  try {
-    await next();
+async function errorHandler() {
+  return async (ctx, next) => {
+    try {
+      await next();
 
-    if (ctx.status === 404) setErrorResponse({ status: 404, message: ctx.response.message }, ctx);
-  } catch (err) {
-    setErrorResponse(err, ctx);
+      if (ctx.status === 404) setErrorResponse({ status: 404, message: ctx.response.message }, ctx);
+    } catch (err) {
+      setErrorResponse(err, ctx);
 
-    ctx.app.emit('error', err, ctx);
-  }
-}
-
-function errorHandler() {
-  return handler;
+      ctx.app.emit('error', err, ctx);
+    }
+  };
 }
 
 module.exports = errorHandler;
