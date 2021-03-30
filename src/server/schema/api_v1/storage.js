4cb10bb8f76f701d2f78e7851ae61b9773456642
Roman-Kirnos <kirnos007@gmail.com>
2021-03-30 09:26:45 +0300

    002.2: commented header authorization

--- src/server/schema/api_v1/storage.js
+++ src/server/schema/api_v1/storage.js
@@ -1,11 +1,11 @@
 const { Joi } = require('koa-joi-router');
 
 const getCustomToken = {
-  header: Joi.object({
-    authorization: Joi.string().pattern(
-      /^Bearer [A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/,
-    ),
-  }).unknown(),
+  // header: Joi.object({
+  //   authorization: Joi.string().pattern(
+  //     /^Bearer [A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/,
+  //   ),
+  // }).unknown(),
 };
 
 module.exports = { getCustomToken };
