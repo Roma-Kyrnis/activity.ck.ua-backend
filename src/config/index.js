40d3a853839d3e2e5f5bb2383fc1411f36eec457
Roman-Kirnos <kirnos007@gmail.com>
2021-04-15 20:48:11 +0300

    002.2-firebase: delete comments

--- src/config/index.js
+++ src/config/index.js
@@ -69,16 +69,6 @@ const config = {
     },
   },
   firebase: {
-    // config: {
-    //   apiKey: process.env.FIREBASE_API_KEY || fatal('No FIREBASE_API_KEY'),
-    //   authDomain: process.env.FIREBASE_AUTH_DOMAIN || fatal('No FIREBASE_AUTH_DOMAIN'),
-    //   projectId: process.env.FIREBASE_PROJECT_ID || fatal('No FIREBASE_PROJECT_ID'),
-    //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET || fatal('No FIREBASE_STORAGE_BUCKET'),
-    //   messagingSenderId:
-    //     process.env.FIREBASE_MESSAGING_SENDER_ID || fatal('No FIREBASE_MESSAGING_SENDER_ID'),
-    //   appId: process.env.FIREBASE_APP_ID || fatal('No FIREBASE_APP_ID'),
-    //   measurementId: process.env.FIREBASE_MEASUREMENT_ID || fatal('No FIREBASE_MEASUREMENT_ID'),
-    // },
     serviceAccount: {
       type: process.env.FIREBASE_TYPE || fatal('No FIREBASE_TYPE'),
       project_id: process.env.FIREBASE_PROJECT_ID || fatal('No FIREBASE_PROJECT_ID'),
