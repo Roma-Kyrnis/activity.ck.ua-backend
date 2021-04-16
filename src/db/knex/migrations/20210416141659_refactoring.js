cc1a1613de03f136245380295285fcdbc7973c9e
Dmytro Tyshchenko <kwazar2008@ukr.net>
2021-04-16 17:27:33 +0300

    001.2: database rating fields refactoring

--- src/db/knex/migrations/20210416141659_refactoring.js
+++ src/db/knex/migrations/20210416141659_refactoring.js
@@ -3,6 +3,11 @@ exports.up = async (knex) => {
     table.timestamp('deleted_at').nullable();
   });
 
+  await knex.schema.alterTable('places', (table) => {
+    table.decimal('rating', 2, 1).notNullable().defaultTo(1.0).alter();
+    table.decimal('popularity_rating', 5, 3).notNullable().defaultTo(1.0).alter();
+  });
+
   await knex.schema.table('reviews', (table) => {
     table.renameColumn('review_text', 'comment');
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
@@ -21,6 +26,11 @@ exports.down = async (knex) => {
   await knex.schema.table('reviews', (table) => {
     table.renameColumn('comment', 'review_text');
     table.dropColumn('created_at');
+  });
+
+  await knex.schema.alterTable('places', (table) => {
+    table.decimal('rating').notNullable().defaultTo(1.0).alter();
+    table.decimal('popularity_rating').notNullable().defaultTo(1.0).alter();
   });
 
   await knex.schema.table('organizations', (table) => {
